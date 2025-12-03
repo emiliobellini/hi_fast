7cfd6719fae91652c39d38fbffc387a3bcb58de5
Emilio Bellini <emilio.bellini@protonmail.com>
2025-12-03 09:59:53 +0100

    .

--- tests/test_params_conversion.py
+++ tests/test_params_conversion.py
@@ -15,7 +15,7 @@ cosmo = HiFast(
 # A_S
 params = {
     'H0': 68.,
-    'Omega_m': 0.3,
+    'Omega_m': 0.31,
     'Omega_b': 0.04,
     'tau_reio': 0.06,
     'ln_A_s_1e10': 3.04,
@@ -40,7 +40,7 @@ pk_class_As = cosmo.get_pk_from_class(
     squeeze=False,
     nonlinear=False,
     precision=1,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
 # HiFast emulated spectra
@@ -49,7 +49,7 @@ cl_emu_As = cosmo.get_cell(
     params,
     name='TT',
     squeeze=False,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
 pk_emu_As = cosmo.get_pk(
@@ -59,31 +59,31 @@ pk_emu_As = cosmo.get_pk(
     name='m',
     squeeze=False,
     nonlinear=False,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
 
 # Sigma8_m
 params = {
     'H0': 68.,
-    'Omega_m': 0.3,
+    'Omega_m': 0.31,
     'Omega_b': 0.04,
     'tau_reio': 0.06,
-    'sigma8_m': 0.837413,
+    'sigma8_m': 0.857265,
     'n_s': 0.966,
 }
 
 # Class reference spectra
-cl_class_s8 = cosmo.get_cell_from_class(
+cl_class_sigma8 = cosmo.get_cell_from_class(
     ref_ell,
     params,
     name='TT',
     squeeze=False,
     precision=1,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
-pk_class_s8 = cosmo.get_pk_from_class(
+pk_class_sigma8 = cosmo.get_pk_from_class(
     ref_k,
     z,
     params,
@@ -91,21 +91,74 @@ pk_class_s8 = cosmo.get_pk_from_class(
     squeeze=False,
     nonlinear=False,
     precision=1,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
 # HiFast emulated spectra
-cl_emu_s8 = cosmo.get_cell(
+cl_emu_sigma8 = cosmo.get_cell(
     ref_ell,
     params,
     name='TT',
     squeeze=False,
     check_params_names=True,
     check_params_values=True,
-    verbose=True,
+    verbose=False,
     timeit=True)
 
-pk_emu_s8 = cosmo.get_pk(
+pk_emu_sigma8 = cosmo.get_pk(
+    ref_k,
+    z,
+    params,
+    name='m',
+    squeeze=False,
+    nonlinear=False,
+    verbose=False,
+    timeit=True)
+
+
+# S8_m
+params = {
+    'H0': 68.,
+    'Omega_m': 0.31,
+    'Omega_b': 0.04,
+    'tau_reio': 0.06,
+    'S8_m': 0.871435629,
+    'n_s': 0.966,
+}
+
+# Class reference spectra
+cl_class_S8 = cosmo.get_cell_from_class(
+    ref_ell,
+    params,
+    name='TT',
+    squeeze=False,
+    precision=1,
+    verbose=False,
+    timeit=True)
+
+pk_class_S8 = cosmo.get_pk_from_class(
+    ref_k,
+    z,
+    params,
+    name='m',
+    squeeze=False,
+    nonlinear=False,
+    precision=1,
+    verbose=False,
+    timeit=True)
+
+# HiFast emulated spectra
+cl_emu_S8 = cosmo.get_cell(
+    ref_ell,
+    params,
+    name='TT',
+    squeeze=False,
+    check_params_names=True,
+    check_params_values=True,
+    verbose=False,
+    timeit=True)
+
+pk_emu_S8 = cosmo.get_pk(
     ref_k,
     z,
     params,
@@ -119,27 +172,35 @@ pk_emu_s8 = cosmo.get_pk(
 # Plot relarive differences
 plt.figure(figsize=(12, 5))
 plt.subplot(1, 2, 1)
-plt.plot(ref_ell, np.abs(cl_emu_s8/cl_class_As-1)*100., label='Emu sigma_8')
+plt.plot(ref_ell, np.abs(cl_emu_As/cl_class_As-1)*100., label='Emu As')
 plt.plot(ref_ell, np.abs(
-    cl_emu_As/cl_class_As-1)*100., '--', label='Emu ln A_s')
+    cl_emu_sigma8/cl_class_As-1)*100., '--', label='Emu sigma8')
 plt.plot(ref_ell, np.abs(
-    cl_class_s8/cl_class_As-1)*100., ':', label='Class sigma_8')
+    cl_emu_S8/cl_class_As-1)*100., ':', label='Emu S8')
+plt.plot(ref_ell, np.abs(
+    cl_class_sigma8/cl_class_As-1)*100., label='Class sigma8')
+plt.plot(ref_ell, np.abs(
+    cl_class_S8/cl_class_As-1)*100., '--', label='Class S8')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('ell')
-plt.ylabel('Relative difference [%]')
+plt.ylabel('C_ell Relative difference [%]')
 plt.title('C_ell (reference: Class ln A_s)')
 plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(ref_k, np.abs(pk_emu_s8/pk_class_As-1)*100., label='Emu sigma_8')
-plt.plot(ref_k, np.abs(pk_emu_As/pk_class_As-1)*100., '--', label='Emu ln A_s')
+plt.plot(ref_k, np.abs(pk_emu_As/pk_class_As-1)*100., label='Emu As')
 plt.plot(ref_k, np.abs(
-    pk_class_s8/pk_class_As-1)*100., ':', label='Class sigma_8')
+    pk_emu_sigma8/pk_class_As-1)*100., '--', label='Emu sigma8')
+plt.plot(ref_k, np.abs(pk_emu_S8/pk_class_As-1)*100., ':', label='Emu S8')
+plt.plot(ref_k, np.abs(
+    pk_class_sigma8/pk_class_As-1)*100., label='Class sigma8')
+plt.plot(ref_k, np.abs(
+    pk_class_S8/pk_class_As-1)*100., '--', label='Class S8')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('k [h/Mpc]')
-plt.ylabel('Relative difference [%]')
+plt.ylabel('P(k) Relative difference [%]')
 plt.title('P(k) (reference: Class ln A_s)')
 plt.legend()
 
